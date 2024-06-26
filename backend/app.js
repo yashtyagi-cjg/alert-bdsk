21551d0ce6bb54d18ffa032ff2ecbfd4dba47af7
yashtyagi-cjg <s2014.moose@gmail.com>
2024-06-27 00:59:53 +0530

    Connecting to DB

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +40,16 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+async function main(){
+  await mongoose.connect(process.env.MONGODB_URI, 
+    {
+      dbName: process.env.MONGODB_NAME
+  })
+
+  console.log('Connected to MongoDB @  ', process.env.MONGODB_NAME);
+}
+
+main().catch((err)=>{
+  console.log(`Encountered error while connecting to DB ${process.env.MONGODB_NAME}`)
+})
 module.exports = app;
