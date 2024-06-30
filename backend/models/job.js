4872659d1f3ce8f4fa964899a65eebda280936e2
yashtyagi-cjg <s2014.moose@gmail.com>
2024-06-30 17:13:05 +0530

    Created to track and schedule notifications to be sent out

--- backend/models/job.js
+++ backend/models/job.js
@@ -5,7 +5,14 @@ const messageJob = new Schema({
     id: { type: String, required: true, unique: true },
     date: { type: Date, required: true },
     oneDayJobId: { type: String, required: true },
-    oneHourJobId: { type: String, required: true }
+    oneHourJobId: { type: String, required: true },
+    message: {type: String, required: true},
+    phoneNumber: {type: String, required: true},
+    status: {
+        type: String,
+        enum: ['sent', 'cancelled', 'rescheduled', 'failed', 'upcoming'],
+        default: 'upcoming',
+    }
 });
 
 module.exports = mongoose.model('messageJob', messageJob);
