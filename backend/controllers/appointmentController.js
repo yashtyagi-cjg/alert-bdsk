8f9360001f4f18e846134b78f0fb5ece6d737292
Aditya Gupta <adityacodeg@gmail.com>
2024-07-01 08:29:51 +0530

    made changes in CRUD functions

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const Appointment = require("./../models/appointment");
+const { scheduler } = require("./../backend/schedulers/messageSchedule.js")
 
 
 /**
@@ -13,14 +14,24 @@ const Appointment = require("./../models/appointment");
  */
 exports.createAppointment = asyncHandler(async (req, res, next) => {
     const appointment = new Appointment({
-        patientID: req.body.patientID,
+        patientId: req.body.patientId._id,
         date: req.body.date,
-        clientID: req.body.clientID,
-        alertID: req.body.alertID,
+        clientId: req.body.clientId._id,
+        // alertId: req.body.alertId,
+        message: `Hi, I am Ramanujan, here to remind you about your upcoming appointment with ${req.body.clientId.name} 
+        @${req.body.date}.`,
         status: req.body.status,
     });
-
-    await appointment.save().catch((err) => {
+    const argAppointment = {
+        id: req.body.clientId._id,
+        date: req.body.clientId.date,
+        phoneNumber: req.body.clientId.phoneNumber,
+        message: req.body.clientId.message,
+    }
+    // Exception handling to be added for scheduleNotifications 
+    await appointment.save().then(() => {
+        scheduler.scheduleNotifications(argAppointment)
+    }).catch((err) => {
         console.log(`ERROR while creating Appointment: ${appointment}`);
         console.log(`ERROR: ${err}`);
     });
@@ -38,7 +49,7 @@ exports.createAppointment = asyncHandler(async (req, res, next) => {
  * @param {Function} next
  */
 exports.getAllAppointments = asyncHandler(async (req, res, next) => {
-    const allAppointments = await Appointment.find({}).exec().catch((err) => {
+    const allAppointments = await Appointment.find({}).populate('clientId').populate('patientId').exec().catch((err) => {
         console.log("ERROR While fetching all appointments");
         console.log(`ERROR: ${err}`);
         return res.status(500).json({ message: "Error encountered while fetching all appointments" });
@@ -48,16 +59,16 @@ exports.getAllAppointments = asyncHandler(async (req, res, next) => {
         return res.status(404).json({ message: "No appointment found." });
     }
 
-    const allAppointmentsFormatted = allAppointments.map((appointment) => {
-        return {
-            _id: appointment._id,
-            patientID: req.body.patientID,
-            date: req.body.date,
-            clientID: req.body.clientID,
-            alertID: req.body.alertID,
-            status: req.body.status,
-        }
-    })
+    // const allAppointmentsFormatted = allAppointments.map((appointment) => {
+    //     return {
+    //         _id: appointment._id,
+    //         patientId: req.body.patientId,
+    //         date: req.body.date,
+    //         clientId: req.body.clientId,
+    //         alertId: req.body.alertId,
+    //         status: req.body.status,
+    //     }
+    // })
 
     res.status(200).json(allAppointmentsFormatted);
     console.log(`SUCCESS: GET ALL APPOINTMENTS`);
@@ -73,11 +84,12 @@ exports.getAllAppointments = asyncHandler(async (req, res, next) => {
  * @param {Function} next
  */
 exports.getAppointmentByID = asyncHandler(async (req, res, next) => {
-    const appointment = await Appointment.findByID(req.params.id).exec().catch((err) => {
-        console.log(`${req.params.id}: Error encountered while fetching appointment`);
-        console.log(`ERROR: ${err}`);
-        return res.status(500).json({ message: "Error encountered while fetching appointment by ID" });
-    });
+    const appointment = await Appointment.findByID(req.params.id).populate('clientId').populate('patientId').exec()
+        .catch((err) => {
+            console.log(`${req.params.id}: Error encountered while fetching appointment`);
+            console.log(`ERROR: ${err}`);
+            return res.status(500).json({ message: "Error encountered while fetching appointment by ID" });
+        });
 
     if (!appointment) {
         console.log(`${req.params.id}: Appointment not found`);
@@ -99,14 +111,39 @@ exports.getAppointmentByID = asyncHandler(async (req, res, next) => {
 exports.updateAppointment = asyncHandler(
     async (req, res, next) => {
         const appointment = {
-            patientID: req.body.patientID,
+            patientId: req.body.patientId._id,
             date: req.body.date,
-            clientID: req.body.clientID,
-            alertID: req.body.alertID,
-            status: req.body.status
+            clientId: req.body.clientId._id,
+            // alertId: req.body.alertId,
+            message: `Hi, I am Ramanujan, here to remind you about your upcoming appointment with ${req.body.clientId.name} @${req.body.date}.`,
+            status: req.body.status,
         }
 
-        await Appointment.findByIDAndUpdate
+        const argAppointment = {
+            id: req.body.clientId._id,
+            date: req.body.clientId.date,
+            phoneNumber: req.body.clientId.phoneNumber,
+            message: req.body.clientId.message,
+        }
+
+        // Delete first, then Update the appointment in messageScheduler 
+
+        await Appointment.findByIdAndDelete(req.params.id).exec().then(() => {
+            scheduler.cancelNotifications(argAppointment)
+        }).catch((err) => {
+            console.log(`${req.params.id}: Error encountered while deleting appointment`);
+            console.log(`ERROR: ${err}`);
+            return res.status(500).json({ message: "Error encountered while deleting appointment" });
+        });
+
+        await appointment.save().then(() => {
+            scheduler.scheduleNotifications(argAppointment)
+        }).catch((err) => {
+            console.log(`ERROR while creating Appointment: ${appointment}`);
+            console.log(`ERROR: ${err}`);
+        });
+
+        res.status(200).json("Appointment details updated successfully");
     }
 )
 
@@ -121,7 +158,17 @@ exports.updateAppointment = asyncHandler(
  */
 exports.deleteAppointment = asyncHandler(
     async (req, res, next) => {
-        await Appointment.findByIdAndDelete(req.params.id).exec().catch((err) => {
+
+        const argAppointment = {
+            id: req.body.clientId._id,
+            date: req.body.clientId.date,
+            phoneNumber: req.body.clientId.phoneNumber,
+            message: req.body.clientId.message,
+        }
+
+        await Appointment.findByIdAndDelete(req.params.id).exec().then(() => {
+            scheduler.cancelNotifications(argAppointment)
+        }).catch((err) => {
             console.log(`${req.params.id}: Error encountered while deleting appointment`);
             console.log(`ERROR: ${err}`);
             return res.status(500).json({ message: "Error encountered while deleting appointment" });
